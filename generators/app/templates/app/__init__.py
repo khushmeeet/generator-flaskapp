4523b76e749444c1b587f35ef3521cf18c32dc5c
khushmeet <khushmeetsingh1996@gmail.com>
2017-09-06 16:23:00 +0530

    defined static, templates and views

---
+++ generators/app/templates/app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, url_for
+from .home.views import home_mod
+
+
+app = Flask(__name__, static_folder='static')
+
+app.register_blueprint(home_mod)
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
